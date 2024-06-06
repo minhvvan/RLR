fb1c96a9a87646d47a51463cdb8733810dfc5cd7
LeeJinWoo <krebye@naver.com>
2024-06-06 19:59:45 +0900

    [Feat] Player Input and Script Configuration
    플레이어 입력 과 관련된 에셋, 스크립트를 구성합니다.

---
+++ Source/RLR/PlayerData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PlayerData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLR_API APlayerData : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
