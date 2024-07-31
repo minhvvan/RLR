3ecd7285ec504c111a354ab2aca9e17db48a2bb5
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-07-31 14:30:36 +0900

    [Bugfix] Modify Talent Key
    FTalent의 Talent관리 Map Key 변경

--- Source/RLR/GameManager/RLRStruct.cpp
+++ Source/RLR/GameManager/RLRStruct.cpp
@@ -403,6 +403,6 @@ void FSkillData::MakeSkillData(Protocol::SkillInfo skill) {
 void FTalent::MakeTalent(Protocol::Talent Data)
 {
     Talents.Add({ 0, TPair<int32,int32>(Data.firsttalent(), Data.firsttalent()) });
-    Talents.Add({ 0, TPair<int32,int32>(Data.secondtalent(), Data.secondlevel()) });
-    Talents.Add({ 0, TPair<int32,int32>(Data.thirdtalent(), Data.thirdlevel()) });
+    Talents.Add({ 1, TPair<int32,int32>(Data.secondtalent(), Data.secondlevel()) });
+    Talents.Add({ 2, TPair<int32,int32>(Data.thirdtalent(), Data.thirdlevel()) });
 }
