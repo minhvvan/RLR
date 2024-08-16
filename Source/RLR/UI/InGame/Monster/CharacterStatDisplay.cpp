ebaca01cd6f45002ec570a7231d6f4749daff5f1
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-08-16 15:11:22 +0900

    [Chore] Modify MonsterHpBar
    재활용을 위해 MonsterHpBar 이름 변경(CharacterStatDisplay)

--- Source/RLR/UI/InGame/Monster/CharacterStatDisplay.cpp
+++ Source/RLR/UI/InGame/Monster/CharacterStatDisplay.cpp
@@ -1,14 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "UI/InGame/Monster/MonsterHpBar.h"
+#include "UI/InGame/Monster/CharacterStatDisplay.h"
 #include "Components/ProgressBar.h"
 #include "Components/TextBlock.h"
+#include "Components/Overlay.h"
 #include "ActionSystem/StatSet/StatSetMonster.h"
 #include "ActionSystem/ActionSystemComponent.h"
 #include "RLRObjects/Characters/RLRMonster.h"
+#include "Blueprint/WidgetTree.h"
+#include "UI/InGame/Monster/AbnormalText.h"
 
-UMonsterHpBar::UMonsterHpBar(const FObjectInitializer& ObjectInitializer):
+UCharacterStatDisplay::UCharacterStatDisplay(const FObjectInitializer& ObjectInitializer):
 	bCompletedChange(true),
 	MaxHp(0),
 	CurrentHp(0)
@@ -16,12 +19,12 @@ UMonsterHpBar::UMonsterHpBar(const FObjectInitializer& ObjectInitializer):
 
 }
 
-void UMonsterHpBar::NativeConstruct()
+void UCharacterStatDisplay::NativeConstruct()
 {
 	Super::NativeConstruct();
 }
 
-void UMonsterHpBar::SetActionSystemComponent(AActor* Owner)
+void UCharacterStatDisplay::SetActionSystemComponent(AActor* Owner)
 {
 	Super::SetActionSystemComponent(Owner);
 
@@ -34,10 +37,19 @@ void UMonsterHpBar::SetActionSystemComponent(AActor* Owner)
 	Name->SetText(FText::FromString(statSet->GetMonsterName()));
 	UpdateHp();
 
-	statSet->OnHpChanged.AddDynamic(this, &UMonsterHpBar::OnHpChanged);
+	statSet->OnHpChanged.AddDynamic(this, &UCharacterStatDisplay::OnHpChanged);
 }
 
-void UMonsterHpBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+void UCharacterStatDisplay::ShowAbnormal(const FString Text)
+{
+	UAbnormalText* abnormalText = WidgetTree->ConstructWidget<UAbnormalText>(AbnormalTextClass);
+	abnormalText->SetAbnormalText(Text);
+
+	OverlayAbnormalText->AddChild(abnormalText);
+	abnormalText->PlayWidgetAnim();
+}
+
+void UCharacterStatDisplay::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
 	Super::NativeTick(MyGeometry, InDeltaTime);
 
@@ -54,13 +66,13 @@ void UMonsterHpBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 	}
 }
 
-void UMonsterHpBar::UpdateHp()
+void UCharacterStatDisplay::UpdateHp()
 {
 	float percent = FMath::Clamp(float(CurrentHp)/MaxHp, 0.f, 100.f);
 	HpProgressBar->SetPercent(percent);
 }
 
-void UMonsterHpBar::OnHpChanged()
+void UCharacterStatDisplay::OnHpChanged()
 {
 	UStatSetMonster* statSet = ActionSystemComponent->GetStatSet<UStatSetMonster>();
 	if (!statSet) return;
--- Source/RLR/UI/InGame/Monster/CharacterStatDisplay.h
+++ Source/RLR/UI/InGame/Monster/CharacterStatDisplay.h
@@ -4,17 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "UI/BaseUI.h"
-#include "MonsterHpBar.generated.h"
+#include "CharacterStatDisplay.generated.h"
 
 class UActionSystemComponent;
+class UOverlay;
+struct FAbnormalMark;
 
 UCLASS()
-class RLR_API UMonsterHpBar : public UBaseUI
+class RLR_API UCharacterStatDisplay : public UBaseUI
 {
 	GENERATED_BODY()
 	
 public:
-	UMonsterHpBar(const FObjectInitializer& ObjectInitializer);
+	UCharacterStatDisplay(const FObjectInitializer& ObjectInitializer);
 
 protected:
 	UPROPERTY(EditAnywhere, meta = (BindWidget))
@@ -23,9 +25,17 @@ protected:
 	UPROPERTY(EditAnywhere, meta = (BindWidget))
 	TObjectPtr<class UTextBlock> Name;
 
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UOverlay> OverlayAbnormalText;
+
+	UPROPERTY(EditAnywhere, Category = UI)
+	TSubclassOf<UUserWidget> AbnormalTextClass;
+
 public:
 	virtual void NativeConstruct() override;
 	void SetActionSystemComponent(AActor* Owner) override;
+
+	void ShowAbnormal(const FString Text);
 
 protected:
 	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
