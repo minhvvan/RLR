67158b0cad9a6e1b2ec21fa71baa28855833c4d9
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-08-21 17:14:03 +0900

    [Feat] Add Get MonsterTransform
    몬스터 id로 위치 받아오는 로직 구현

--- Source/RLR/GameManager/MonsterManager.h
+++ Source/RLR/GameManager/MonsterManager.h
@@ -32,6 +32,8 @@ public:
     UFUNCTION()
     void SpawnMonsters();
 
+    const FVector GetMonsterTranformById(int MonsterId);
+
 private:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
     TArray<FMonsterStatus> Monsters;
