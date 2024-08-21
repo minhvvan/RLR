a12ed9aa51e7b2eb29ed92b9d2f4a7899ef62dea
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-08-21 15:47:11 +0900

    [Feat] Spawn DropItem
    DropItem Spawn 및 획득 요청, 결과 처리 로직 구현

--- Source/RLR/GameManager/ObjectManager.h
+++ Source/RLR/GameManager/ObjectManager.h
@@ -9,6 +9,7 @@
 
 class ARLRNonPlayerCharacter;
 class ARLRInteractableActor;
+class ARLRDropItem;
 
 UCLASS()
 class RLR_API UObjectManager : public UGameInstanceSubsystem
@@ -22,15 +23,33 @@ public:
     UFUNCTION()
     void SetNPCData(TArray<FNPCData> Data);
 
-    UFUNCTION()
-    void SpawnNPC();
-
     //Object
     UFUNCTION()
     void SetObjectData(TArray<FInteractData> Data);
 
+    //DropItem
+    UFUNCTION()
+    void SetDropItemData(TArray<FDropItem> Data);
+
+    //아이템 획득 요청
+    UFUNCTION()
+    void RequestPickUpItem(const FDropItem& Dropitem);
+
+    //아이템 획득 요청 결과 처리
+    UFUNCTION()
+    void ResponePickUpItem(int ObjectId, bool bSuccess);
+
+protected:
+    UFUNCTION()
+    void SpawnNPC();
+
     UFUNCTION()
     void SpawnObejct();
+
+    UFUNCTION()
+    void SpawnDropItem();
+
+    TObjectPtr<ARLRDropItem> GetObjectInstanceById(int ObjectId);
 
 private:
     //NPC
@@ -54,4 +73,13 @@ private:
     TMap<EInteractObjectType,TSubclassOf<ARLRInteractableActor>> ObjectClasses;
 
     FCriticalSection ObjectDataMutex;
+
+    //Item
+    UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+    TArray<FDropItem> DropItemData;
+
+    UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+    TArray<TObjectPtr<ARLRDropItem>> DropItemInstances;
+
+    FCriticalSection ItemDataMutex;
 };
