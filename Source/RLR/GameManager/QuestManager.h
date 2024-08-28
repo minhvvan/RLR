79455755851746ed2965cfe52d17495ee84150f3
MinkyeongYu <y071812@gmail.com>
2024-08-28 13:53:51 +0900

    [Feat] Add Required Files for QuestSystem

---
+++ Source/RLR/GameManager/QuestManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "QuestManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLR_API UQuestManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
