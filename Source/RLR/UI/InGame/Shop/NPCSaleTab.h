65ba2f59e4e1917c5d303fcb0c008397652a7041
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-09-04 14:46:39 +0900

    [Feat] Add NPCShop SaleTab
    NPC상점 판매 탭 클래스 추가

---
+++ Source/RLR/UI/InGame/Shop/NPCSaleTab.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseUI.h"
+#include "NPCSaleTab.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLR_API UNPCSaleTab : public UBaseUI
+{
+	GENERATED_BODY()
+	
+};
