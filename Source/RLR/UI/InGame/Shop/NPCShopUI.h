0503066aea75ab866de785ca10a294c568b91e7d
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-09-04 14:45:12 +0900

    [Feat] Add NPCShopUI
    NPC 상점 UI 클래스 추가

---
+++ Source/RLR/UI/InGame/Shop/NPCShopUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SubUI.h"
+#include "NPCShopUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLR_API UNPCShopUI : public USubUI
+{
+	GENERATED_BODY()
+	
+};
