c890434cab4d1092cbab13843c18e4a42dc07db6
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-08 13:26:21 +0900

    [Feat] Add Report UI

---
+++ Source/RLR/UI/InGame/OtherUser/ReportUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SubUI.h"
+#include "ReportUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLR_API UReportUI : public USubUI
+{
+	GENERATED_BODY()
+	
+};
