639f8504fe73e1e95e4e11cf4a81528b0f8653e5
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-08 16:14:34 +0900

    [Feat] Report Submit
    신고 내용, 타입 받아오기

--- Source/RLR/UI/InGame/OtherUser/ReportUI.h
+++ Source/RLR/UI/InGame/OtherUser/ReportUI.h
@@ -6,12 +6,45 @@
 #include "UI/SubUI.h"
 #include "ReportUI.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class UMultiLineEditableText;
+class UComboBoxString;
+
 UCLASS()
 class RLR_API UReportUI : public USubUI
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeConstruct() override;
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidgetOptional))
+	TObjectPtr<UButton> BtnExit;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidgetOptional))
+	TObjectPtr<UTextBlock> TxtUserName;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidgetOptional))
+	TObjectPtr<UComboBoxString> CboxReportType;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidgetOptional))
+	TObjectPtr<UMultiLineEditableText> EdtDetail;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidgetOptional))
+	TObjectPtr<UButton> BtnSubmit;
+
+protected:
+	UFUNCTION()
+	void OnSubmitClicked();
+
+	void OpenUI() override;
+
+public:
+	void SetUserName(FString NickName);
+	void SetUserSeq(int Seq);
+
+private:
+	int UserSeq;
 };
