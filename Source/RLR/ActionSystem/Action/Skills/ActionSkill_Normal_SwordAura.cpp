435a8274a2bdedb5a30255f35cbec37ce1b00472
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-10-15 16:12:13 +0900

    [Refactor] Modify Skill_Aura Dir
    Skill_Aura 방향 조정

--- Source/RLR/ActionSystem/Action/Skills/ActionSkill_Normal_SwordAura.cpp
+++ Source/RLR/ActionSystem/Action/Skills/ActionSkill_Normal_SwordAura.cpp
@@ -29,35 +29,25 @@ void UActionSkill_Normal_SwordAura::ActivateAction()
 	UActionSystemComponent* ASC = Player->GetActionSystemComponent();
 	if (!ASC) return;
 
+	if (!SkillData) return;
+
 	FActionData ActionData;
 	ASC->GetActionData(TriggerTag, ActionData);
 
 	FVector MousePos = ActionData.MousePos;
 
-	//Test
-	if (!SkillData)
-	{
-
-		UE_LOG(LogTemp, Log, TEXT("asdsagfdg"));
-		return;
-	}
-
-	float SkillRange = 1000.f;
-	/*FVector StartPos = Player->GetActorLocation();
-	FVector SkillDir = (MousePos - StartPos);*/
-	FRotator playerRotation = Player->GetActorRotation();
-	playerRotation.Pitch = 0.f;
-	/*FRotator Rotator = FRotationMatrix::MakeFromX(SkillDir).Rotator();
-	Rotator.Pitch = 0.f;*/
-
+	float SkillRange = SkillData->SkillDistance;
+	FVector StartPos = Player->GetActorLocation();
+	FVector SkillDir = (MousePos - StartPos);
+	FRotator Rotator = SkillDir.ToOrientationRotator();
+	Rotator.Pitch = 0.f;
 
 	ARLRProjectile* Aura = GetWorld()->SpawnActorDeferred<ARLRProjectile>(SwordAuraProjectile, FTransform::Identity, Player);
-	/*Aura->SetFireDir();*/
 	Aura->SetSkillRange(SkillRange);
 	Aura->OnFinishSkill.AddDynamic(this, &UActionSkill_Normal_SwordAura::OnFinishSkill);
 
 	FTransform SpawnLoc(Player->GetActorLocation() + Player->GetActorForwardVector() * 50);
-	SpawnLoc.SetRotation(playerRotation.Quaternion());
+	SpawnLoc.SetRotation(Rotator.Quaternion());
 
 	Aura->FinishSpawning(SpawnLoc);
 }
@@ -65,15 +55,9 @@ void UActionSkill_Normal_SwordAura::ActivateAction()
 void UActionSkill_Normal_SwordAura::OnFinishSkill(TArray<AActor*> OverlappedActor)
 {
 	USkillManager* SkillManager = GameInstance->GetSkillManager();
-	if (!SkillManager) return;
-
-	if (SkillManager->RequestSkillResult(SkillData, OverlappedActor))
+	if (SkillManager)
 	{
-		//Success
-	}
-	else
-	{
-		//fail
+		SkillManager->RequestSkillResult(SkillData, OverlappedActor);
 	}
 
 	EndAction();
