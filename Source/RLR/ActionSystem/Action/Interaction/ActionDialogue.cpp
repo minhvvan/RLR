f844e5f04d709c3f5f4d4f8d2e938c3903b952ab
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-11-08 13:43:19 +0900

    [BugFix] Stop Movement when Dialogue Started

--- Source/RLR/ActionSystem/Action/Interaction/ActionDialogue.cpp
+++ Source/RLR/ActionSystem/Action/Interaction/ActionDialogue.cpp
@@ -6,6 +6,7 @@
 #include "GameManager/GameManager.h"
 #include "GameManager/GameplayTagManager.h"
 #include "ActionSystem/ActionSystemComponent.h"
+#include "RLRObjects/Characters/RLRPlayerCharacter.h"
 #include "UI/DialogueUI.h"
 #include "RLR.h"
 
@@ -25,6 +26,15 @@ void UActionDialogue::ActivateAction()
 {
 	UActionSystemComponent* playerASC = CurrentActorInfo->ActionSystemComponent.Get();
 	if (!playerASC) return;
+
+	ARLRPlayerCharacter* Player = Cast<ARLRPlayerCharacter>(GetAvatarActorFromActorInfo());
+	if (!Player) return;
+
+	AController* Controller = Player->GetController();
+	if (Controller)
+	{
+		Controller->StopMovement();
+	}
 
 	FGameplayTagManager TagManager = FGameplayTagManager::Get();
 	FActionData actionData;
