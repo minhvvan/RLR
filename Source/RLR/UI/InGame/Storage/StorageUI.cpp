8a9d28da4c55941e8a7b1f282df8577359c0e93b
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2024-11-22 16:03:47 +0900

    [Feat] Set StorageItems
    창고 아이템 초기화 함수 구현

--- Source/RLR/UI/InGame/Storage/StorageUI.cpp
+++ Source/RLR/UI/InGame/Storage/StorageUI.cpp
@@ -2,8 +2,57 @@
 
 
 #include "UI/InGame/Storage/StorageUI.h"
+#include "UI/InGame/Storage/StorageTab.h"
+#include "UI/Components/WidgetSwitcherButton.h"
+#include "Structs/ItemStructs.h"
+#include "Components/WidgetSwitcher.h"
+#include "Components/Button.h"
+#include "Components/HorizontalBox.h"
+#include "Blueprint/WidgetTree.h"
+#include "RLR.h"
+#include "GameManager/DataManager.h"
+#include "GameManager/GameManager.h"
+
 
 void UStorageUI::NativeConstruct()
 {
+	Super::NativeConstruct();
 
+	//BtnBox에 존재하는 WidgetSwitcherBtn 저장
+	TabButtons.Empty();
+	WidgetTree->GetChildWidgets(BtnBox, TabButtons);
+}
+
+//창고 Open시 모든 아이템 Slot 설정
+void UStorageUI::SetStorageItems(const TArray<TArray<FItemData>>& StorageItems)
+{
+	Items = StorageItems;
+	SetUnLockedPageNum();
+	RefreshUI();
+}
+
+void UStorageUI::SetUnLockedPageNum()
+{
+	UnLockedPageNum = Items.Num();
+
+	for (int i = 0; i < MaxStoragePageNum; i++)
+	{
+		if (i < UnLockedPageNum) TabButtons[i]->SetIsEnabled(true);
+		else TabButtons[i]->SetIsEnabled(false);
+	}
+}
+
+void UStorageUI::RefreshUI()
+{
+	for (int i = 0; i < Items.Num(); i++)
+	{
+		UStorageTab* CurrentTab = Cast<UStorageTab>(WidgetSwitcher->GetWidgetAtIndex(i));
+		if (!CurrentTab)
+		{
+			RLR_LOG(LogRLR, Log, TEXT("StorageTab 형식 불일치"));
+			continue;
+		}
+
+		CurrentTab->UpdateAllItem(Items[i]);
+	}
 }
--- Source/RLR/UI/InGame/Storage/StorageUI.h
+++ Source/RLR/UI/InGame/Storage/StorageUI.h
@@ -6,14 +6,36 @@
 #include "UI/SubUI.h"
 #include "StorageUI.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UWidgetSwitcher;
+class UWidgetSwitcherButton;
+class UHorizontalBox;
+
 UCLASS()
 class RLR_API UStorageUI : public USubUI
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UHorizontalBox> BtnBox;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+
 public:
 	virtual void NativeConstruct() override;
+
+public:
+	void SetStorageItems(const TArray<TArray<FItemData>>& StorageItems);
+	void SetUnLockedPageNum();
+
+protected:
+	TArray<UWidget*> TabButtons;
+	TArray<TArray<FItemData>> Items;
+
+	int UnLockedPageNum = 1;
+	const int MaxStoragePageNum = 3;
+
+	virtual void RefreshUI() override;
 };
