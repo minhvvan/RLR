10bd80e5925d5c66cadfec399bae741d488db875
MinkyeongYu <y071812@gmail.com>
2024-12-16 17:54:34 +0900

    [Feat] AddFriendMessageBox

---
+++ Source/RLR/UI/InGame/FriendList/Popup/AddFriendMessageBox.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/InGame/FriendList/Popup/AddFriendMessageBox.h"
+
+void UAddFriendMessageBox::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
---
+++ Source/RLR/UI/InGame/FriendList/Popup/AddFriendMessageBox.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/MessageBoxUI.h"
+#include "AddFriendMessageBox.generated.h"
+
+class UComboBoxString;
+class UEditableText;
+/**
+ * 친구 추가 시 뜨는 팝업
+ */
+UCLASS()
+class RLR_API UAddFriendMessageBox : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* GroupListDropDownBox;
+
+	UPROPERTY(meat = (BindWidget))
+	UEditableText* PlayerNameInputText;
+};
