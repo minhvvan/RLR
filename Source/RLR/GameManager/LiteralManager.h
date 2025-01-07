921c3819745d2fde9f5b173a078455d0b8e50317
Lee Minhwan <59609086+minhvvan@users.noreply.github.com>
2025-01-08 00:15:51 +0900

    [Feat] Add SkillStruct Literal
    LiteralManager::Get에서 const 제거

--- Source/RLR/GameManager/LiteralManager.h
+++ Source/RLR/GameManager/LiteralManager.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 
+enum class ECostType : uint8;
+enum class ESkillType : uint8;
+
 /* GameplayTagManager */
 struct FTagPair
 {
@@ -24,8 +27,11 @@ struct FTagPair
 class RLR_API LiteralManager
 {
 public:
-	static const LiteralManager& Get() { return LiteralMessages; }
+	static LiteralManager& Get() { return LiteralMessages; }
 	static void Init();
+
+	FString GetSkillTypeText(ESkillType type);
+	FString GetSkillCostText(ECostType type);
 
 public:
 	/* TradeUI */
@@ -58,6 +64,15 @@ public:
 	/* SkillUI */
 	FString SkillUI_Learn;
 	FString SkillUI_Upgrade;
+	FString SkillType_Normal;
+	FString SkillType_Area;
+	FString SkillType_Holding;
+	FString SkillType_Casting;
+	FString SkillType_Mobility;
+	FString SkillType_Targeting;
+	FString SkillType_Chain;
+	FString CostType_MP;
+	FString CostType_HP;
 
 	/* Player Commands */
 	FString Playercommands_InputConfigNull;
